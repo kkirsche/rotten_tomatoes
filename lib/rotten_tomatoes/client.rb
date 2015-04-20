8a0e70a2404fc7ca96fac6248792fdb3b4fac2e1
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-04-19 22:25:55 -0400

    Implement First 8 Methods
    Movies Search
    Lists Directory
    Movie Lists Directory
    Box Office Movies
    In Theaters Movies
    Opening Movies
    Upcoming Movies
    DVD Lists Directory

--- lib/rotten_tomatoes/client.rb
+++ lib/rotten_tomatoes/client.rb
@@ -9,34 +9,60 @@ module RottenTomatoes
       @client = Hurley::Client.new 'http://api.rottentomatoes.com/api/public/v1.0'
     end
 
-    def movies_search(args = {})
-      response = @client.get('movies.json', {}.tap do |hash|
+    def argument_hash(args = {}, symbols = [])
+      {}.tap do |hash|
         hash[:apikey] = @api_key
-        [:q, :page_limit, :page].map do |sym|
+        symbols.map do |sym|
           hash[sym] = args[sym] unless args[sym].nil?
         end
-      end)
+      end
+    end
+
+    def movies_search(args = {})
+      response = @client.get('movies.json',
+                             argument_hash(args, [:q, :page_limit, :page]))
 
       JSON.parse(response.body)
     end
 
     def lists_directory
-      response = @client.get('lists.json', apikey: @api_key)
+      response = @client.get('lists.json', argument_hash)
       JSON.parse(response.body)
     end
 
     def movie_lists_directory
-      response = @client.get('lists/movies.json', apikey: @api_key)
+      response = @client.get('lists/movies.json', argument_hash)
+      JSON.parse(response.body)
+    end
+
+    def dvd_lists_directory
+      response = @client.get('lists/dvds.json', argument_hash)
       JSON.parse(response.body)
     end
 
     def box_office_movies(args = {})
-      response = @client.get('lists/movies/box_office.json', {}.tap do |hash|
-        hash[:apikey] = @api_key
-        [:limit, :country].map do |sym|
-          hash[sym] = args[sym] unless args[sym].nil?
-        end
-      end)
+      response = @client.get('lists/movies/box_office.json',
+                             argument_hash(args, [:limit, :country]))
+      JSON.parse(response.body)
+    end
+
+    def in_theater_movies(args = {})
+      response = @client.get('lists/movies/in_theaters.json',
+                             argument_hash(args,
+                                           [:page_limit, :page, :country]))
+      JSON.parse(response.body)
+    end
+
+    def opening_movies(args = {})
+      response = @client.get('lists/movies/opening.json',
+                             argument_hash(args, [:limit, :country]))
+      JSON.parse(response.body)
+    end
+
+    def upcoming_movies(args = {})
+      response = @client.get('lists/movies/upcoming.json',
+                             argument_hash(args,
+                                           [:page_limit, :page, :country]))
       JSON.parse(response.body)
     end
   end
