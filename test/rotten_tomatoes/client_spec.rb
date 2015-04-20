a361c643d4412db86926890bbc2e9f4c270c7680
Kevin Kirsche <kev.kirsche@gmail.com>
2015-04-20 09:10:37 -0400

    Fix API Key assignment in tests

--- test/rotten_tomatoes/client_spec.rb
+++ test/rotten_tomatoes/client_spec.rb
@@ -6,15 +6,15 @@ module RottenTomatoesTest
     describe 'Rotten Tomatoes API Library',
              'The client used to interact with the Rotten Tomatoes API' do
       it 'should initialize and set defaults without error' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         assert_kind_of RottenTomatoes::Client, client
-        expect(client.api_key).must_equal api_key: 'myAPIkey'
+        expect(client.api_key).must_equal 'myAPIkey'
         client.wont_be_nil
       end
 
       it 'should search for movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/movies.json' do
@@ -30,7 +30,7 @@ module RottenTomatoesTest
       end
 
       it 'should list directories' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists.json' do
@@ -44,7 +44,7 @@ module RottenTomatoesTest
       end
 
       it 'should list movie directories' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/movies.json' do
@@ -58,7 +58,7 @@ module RottenTomatoesTest
       end
 
       it 'should list dvd directories' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/dvds.json' do
@@ -72,7 +72,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve box office movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/movies/box_office.json' do
@@ -86,7 +86,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve in theater movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/movies/in_theaters.json' do
@@ -100,7 +100,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve opening movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/movies/opening.json' do
@@ -114,7 +114,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve upcoming movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/movies/upcoming.json' do
@@ -128,7 +128,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve top rented movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/dvds/top_rentals.json' do
@@ -142,7 +142,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve currently released on disc movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/dvds/current_releases.json' do
@@ -156,7 +156,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve newly released on disc movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/dvds/new_releases.json' do
@@ -170,7 +170,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve upcoming on disc movies' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/lists/dvds/upcoming.json' do
@@ -184,7 +184,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve a movie\'s information' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/movies/770672122.json' do
@@ -198,7 +198,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve a movie\'s cast' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/movies/770672122/cast.json' do
@@ -212,7 +212,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve a movie\'s reviews' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/movies/770672122/reviews.json' do
@@ -230,7 +230,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve a movies which are similar to a movie' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/movies/770672122/similar.json' do
@@ -244,7 +244,7 @@ module RottenTomatoesTest
       end
 
       it 'should retrieve a movie\'s aliases' do
-        client = RottenTomatoes::Client.new api_key: 'myAPIkey'
+        client = RottenTomatoes::Client.new 'myAPIkey'
 
         client.client.connection = Hurley::Test.new do |test|
           test.get '/api/public/v1.0/movie_alias.json' do
